aa369f83800e3bcc92dc1ba73d76d9c5ddf903dd
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-04-13 17:30:51 +0100

    added styling and function for basic layout

--- script.js
+++ script.js
@@ -6,7 +6,24 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  for (let episode of episodeList) {
+    const episodeDiv = document.createElement("div");
+    episodeDiv.classList.add("episode");
 
+    const episodeTitle = document.createElement("h2");
+    episodeTitle.textContent = `${episode.name} - S${String(episode.season).padStart(2, "0")}E${String(episode.number).padStart(2, "0")}`;
+    episodeDiv.appendChild(episodeTitle);
+
+    const episodeImage = document.createElement("img");
+    episodeImage.src = episode.image.medium;
+    episodeDiv.appendChild(episodeImage);
+
+    const episodeSummary = document.createElement("p");
+    episodeSummary.innerHTML = episode.summary;
+    episodeSummary.classList.add("summary");
+    episodeDiv.appendChild(episodeSummary);
+
+    rootElem.appendChild(episodeDiv);
+  }
+}
 window.onload = setup;
